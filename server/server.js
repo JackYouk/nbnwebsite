95a7100837edf353182a68f6ff253f629e0d0402
JackYouk <jackyoukstetter1@gmail.com>
2022-10-11 21:35:34 -0700

    favicon

--- server/server.js
+++ server/server.js
@@ -2,14 +2,12 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-const PORT = 3055 || process.env.PORT;
+const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
-}
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/build/index.html'));
